f9db74be45fc71b97a19298278fcd558943b3e28
Dean B <deantburrows88@gmail.com>
2024-12-18 09:34:42 +0100

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -44,6 +44,7 @@ function addIdealoButton(titleElement) {
     text-decoration: none;
     border-radius: 5px;
     font-weight: bold;
+    font-size: 16px
   `;
   titleElement.parentElement.appendChild(idealoButton);
 }
